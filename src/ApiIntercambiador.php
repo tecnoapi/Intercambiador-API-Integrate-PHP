5a4fd8d8bc2831d6be1db28f19190e48bfdb29eb
Blanca Mir <bmir@tridenia.com>
2022-05-23 13:26:13 +0200

    first commit

---
+++ src/ApiIntercambiador.php
@@ -0,0 +1,113 @@
+<?php 
+namespace ApiIntercambiador;
+
+class ApiIntercambiador
+{
+    private $token;
+    private $sandbox;
+
+    public function __construct($token, $sandbox=null)
+    {
+        $this->token = $token;
+        $this->sandbox = $sandbox;
+    }
+
+    // $api = new ApiIntercambiador($token) => REAL
+
+    // $api = new ApiIntercambiador($token, 'sandbox') //=> SANDBOX
+
+    // $result = $api->getProperties(); => GET /api-intercambiador 
+
+    // $result = $api->addProperty($array_data); => POST /properties
+
+    // $result = $api->updateProperty($array_data); => PUT /properties
+
+    // $result = $api->removeProperty($array_id); => DELETE /properties
+
+    
+
+    // $array_data és un array de properties, pot ser que només vungui 1 property
+
+    // $array_id és un array d’IDs de properties, pot ser que només vingui 1 ID
+    
+    public function getProperties()
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $_ENV["APP_URL"].'/api-intercambiador',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            'x-access-token: ' . $this->token,
+            'Content-Type: application/json'
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+
+        return $response;
+    }
+
+    
+    public function addProperty($array_data)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $_ENV["APP_URL"].'/properties',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $array_data,
+        CURLOPT_HTTPHEADER => array(
+            'x-access-token: ' . $this->token,
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+
+
+    public function updateProperty($array_data)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $_ENV["APP_URL"].'/properties/'.$array_data['id'],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'PUT',
+        CURLOPT_POSTFIELDS => $array_data,
+        CURLOPT_HTTPHEADER => array(
+            'x-access-token: ' . $this->token,
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+}
